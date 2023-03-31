58371f3d6445e328717e35f92ba7cc63142c5302
rm93897 <rm93897@fiap.com.br>
2023-03-31 09:47:36 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Produto produto;
+
+        boolean encerrar = false;
+        String nome = "";
+        double valor = 0;
+        double total = 0;
+
+        do{
+            nome = JOptionPane.showInputDialog("Nome do produto");
+
+            valor = Double.parseDouble(JOptionPane.showInputDialog("Valor do produto (apenas números)"));
+
+            produto = new Produto(nome, valor);
+
+            System.out.println(produto);
+            total = total + produto.getValor();
+
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog("Deseja mais alguma coisa?" +
+                    "(1) = SIM" +
+                    "(0) = NÃO"));
+
+            encerrar = opcao == 1 ? false : true;
+
+        }while(encerrar == false);
+
+        System.out.printf("Total da Compra: R$ %,2%n" + total);
+
+    }
+}
